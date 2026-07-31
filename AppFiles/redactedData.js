0114fd5c02103435bbffcce8af0208e5297b337c
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2026-07-31 10:33:10 +0100

    Add files via upload

--- AppFiles/redactedData.js
+++ AppFiles/redactedData.js
@@ -1,12 +1,9 @@
 const DATA = [
 
-// Statmaxing
-  { text: "Install beliefs by focusing on them pre-sleep.", category: "Statmaxing", setting: "Social", spice: 0 },
- 
-// STORY
-  { text: "Tell a heroic story, but comically demean yourself, to show your heroic side without being braggy.", category: "Story", setting: "Social", spice: 0 },
-
   //Truism
-  { text: "Scarcity creates value.", category: "Truism", setting: "One on One", spice: 1 }
+
+  { text: "Scarcity creates value.", category: "Truism", setting: "One on One", spice: 1, boldness: 1 , pertinence: 1 , time: "Morning"},
+  { text: "Scarcity creates value.", category: "Truism", setting: "One on One", spice: 1, boldness: 1 , pertinence: 1 , time: "Morning"},
+  { text: "Scarcity creates value.", category: "Truism", setting: "One on One", spice: 1, boldness: 1 , pertinence: 1 , time: "Morning"},
 
   ];
